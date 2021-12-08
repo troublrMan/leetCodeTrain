a61d6056d741835ec14eeae864aaacafe4eb7978
wangchao <wangchao@yupaopao.cn>
2021-12-08 23:28:33 +0800

    init

--- src/main/java/com/trouble/train/DemoClass.java
+++ src/main/java/com/trouble/train/DemoClass.java
@@ -1,5 +1,10 @@
 package com.trouble.train;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * @author troubleMan
  * @description dem方法
@@ -8,10 +13,10 @@ package com.trouble.train;
  **/
 public class DemoClass {
 
-    public static void main(String[] args) {
-        int [] array = {33, 22, 1, 4, 25, 88, 71, 4};
-        insertionSort(array);
-    }
+//    public static void main(String[] args) {
+//        int [] array = {33, 22, 1, 4, 25, 88, 71, 4};
+//        insertionSort(array);
+//    }
     /**
      * 插入排序
      */
@@ -28,18 +33,67 @@ public class DemoClass {
                 }
             }
             array[j+1] = value;
-            System.out.println(array);
+        }
+        for(int i : array) {
+            System.out.print(i + " ");
         }
     }
 
     /**
      * 打印数组
-     * @param array
+
      */
+
+    public static void main(String[] args) {
+//        List<Student> students = new ArrayList<>(3);
+//        students.add(new Student("路飞", 22, 175));
+//        students.add(new Student("红发", 40, 180));
+//        students.add(new Student("白胡子", 50, 185));
+//
+//        long count = students.stream().filter(s1 -> s1.getAge() < 45).count();
+//        System.out.println("年龄小于45岁的人数是：" + count);
+//
+//        Integer reduce = Stream.of(1, 2, 3, 4).reduce(0, (acc, x) -> acc+ x);
+//        System.out.println(reduce);
+
+
+        List<Student> students = new ArrayList<>(3);
+        students.add(new Student("路飞", 22, 175));
+        students.add(new Student("红发", 40, 180));
+        students.add(new Student("白胡子", 50, 185));
+
+        String names = students.stream()
+                .map(Student::getName).collect(Collectors.joining(",","[","]"));
+        System.out.println(names);
+    }
     private static void print(int[] array) {
-        for(int i : array) {
-            System.out.print(i + " ");
+
+
+
+    }
+
+    static class Student{
+        String name;
+        Integer age;
+        Integer hight;
+
+        public String getName() {
+            return name;
         }
-        System.out.println();
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public Integer getHight() {
+            return hight;
+        }
+
+        public Student(String name, Integer age, Integer hight) {
+            this.name = name;
+            this.age = age;
+            this.hight = hight;
+        }
     }
 }
+
